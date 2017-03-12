883cba14fbc8288dfd03374b0cd404c4636831c4
Jagie <chen_cwf@163.com>
2017-03-12 15:38:16 +0800

    Initial Commit

---
+++ README.md
@@ -0,0 +1,24 @@
+#SFAudioWaveformHelper
+
+
+This project illustrates how to use Accelerate framework and generate the waveform image of an audio file
+
+![ScreenShot](https://raw.github.com/JagieChen/SFAudioWaveformHelper/master/s1.png)
+
+The code is derived from https://github.com/fulldecent/FDWaveformView and enlightened from http://www.davidstarke.com/2015/04/waveforms.html
+
+
+
+## Features
+
+* Using Swift 3;
+* Using Accelerate framework;
+* I keep the code as concise as I can.
+
+## License
+
+This code is distributed under the terms and conditions of the [MIT license](LICENSE).
+
+
+
+
---
+++ SFAudioWaveformHelper/ViewController.swift
@@ -0,0 +1,50 @@
+//
+//  ViewController.swift
+//  SFAudioWaveformHelper
+//
+//  Created by CHENWANFEI on 12/03/2017.
+//  Copyright © 2017 SwordFish. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+    @IBOutlet weak var imageView: UIImageView!
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+    @IBAction func onGenerate(_ sender: Any) {
+        let now = Date.timeIntervalSinceReferenceDate;
+        let audioURL = Bundle.main.url(forResource: "audio", withExtension: "mp3")!;
+        generateWaveformImage(audioURL: audioURL, imageSizeInPixel: imageView.bounds.size, waveColor: UIColor.red) {[weak self] (waveFormImage) in
+            if let waveFormImage = waveFormImage {
+            
+                self?.imageView.image = waveFormImage;
+                
+                self?.alert(str: "Seconds costed:\(Date.timeIntervalSinceReferenceDate - now)");
+                
+            }else{
+                self?.alert(str: "Shit happens");
+            }
+        }
+    }
+    
+    
+    private func alert(str:String){
+        let ac = UIAlertController(title: "Result", message: str, preferredStyle: UIAlertControllerStyle.alert);
+        ac.addAction(UIAlertAction(title: "OK", style: UIAlertActionStyle.default, handler: { (_) in
+            ac.dismiss(animated: true, completion: nil);
+        }))
+        self.present(ac, animated: true, completion: nil);
+    }
+
+}
+
